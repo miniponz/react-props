4e2e572b14d2eb669a0664637b269c819ba87873
Cara Ponzini <ponzini@gmail.com>
2019-05-14 09:25:18 -0700

    array works

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
 
-const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'purple'];
 render(
   <App colors={colors}/>,
   document.getElementById('root')
